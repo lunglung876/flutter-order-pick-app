74d2c4bbd1a4877f8fe01d616e387fdffb498d45
Eric Lai <eric.lai@101medialab.com>
2019-05-29 18:18:32 +0800

    temp

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/services.dart';
 
 import 'package:warehouse_order_pick/authentication/authentication.dart';
 
@@ -7,21 +8,39 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AuthenticationBloc authenticationBloc =
-    BlocProvider.of<AuthenticationBloc>(context);
+        BlocProvider.of<AuthenticationBloc>(context);
+
+    FocusNode _textNode = new FocusNode();
+    TextEditingController _controller = new TextEditingController();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
       body: Container(
-        child: Center(
-            child: RaisedButton(
-              child: Text('logout'),
-              onPressed: () {
-                authenticationBloc.dispatch(LoggedOut());
+          child: new RawKeyboardListener(
+            focusNode: _textNode,
+            onKey: handleKey,
+            child: Builder(
+              builder: (BuildContext context) {
+                FocusScope.of(context).requestFocus(_textNode);
+                return new Container();
               },
-            )),
-      ),
+            ),
+          )
+        )
     );
+  }
+
+  handleKey(input) {
+    if (input is RawKeyDownEvent) {
+      const int combiningCharacterMask = 0x7fffffff;
+      // ignore:
+        final String codePointChar = String.fromCharCode(
+            combiningCharacterMask & input.data.codePoint);
+      // ignore: all
+        debugPrint('codePoint: ${input.data.codePoint} (${_asHex(
+            input.data.codePoint)}: $codePointChar)');
+    }
   }
 }
