637d4c74dc2988761e74ac6a67fb48ae37e35d7c
Eric Lai <eric.lai@101medialab.com>
2019-06-21 14:23:43 +0800

    Sort items by location

--- lib/items/items_bloc.dart
+++ lib/items/items_bloc.dart
@@ -19,6 +19,36 @@ class ItemsBloc extends Bloc<ItemsEvent, ItemsState> {
       try {
         final items = await api.getItems(event.orderNumbers);
 
+        // Sort items by shelf. The order is TIMES > A > AA > Z > Z8.
+        items.sort((a, b) {
+          RegExp regex = RegExp('^(TIMES|Z8|Z\/|[A-Z]{1,2})');
+          Match areaMatchesA = regex.firstMatch(a.shelf);
+          Match areaMatchesB = regex.firstMatch(b.shelf);
+
+          if (null == areaMatchesA || null == areaMatchesB) {
+            return 0;
+          }
+
+          String areaA = areaMatchesA.group(0);
+          String areaB = areaMatchesB.group(0);
+
+          if (areaA == 'TIMES') {
+            return -1;
+          } else if (areaB == 'TIMES') {
+            return 1;
+          }
+
+          if (areaA == areaB) {
+            return a.shelf.compareTo(b.shelf);
+          }
+
+          if (areaA.length != areaB.length) {
+            return (areaA.length < areaB.length ? -1 : 1);
+          }
+
+          return areaA.compareTo(areaB);
+        });
+
         yield ItemsLoaded(items: items);
       } catch (e) {
         yield ItemsError(error: e.message);
